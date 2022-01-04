8cf6da0f064308741b06e9e5b5f87b449b604ea0
Ayush Aggarwal <Aama098@gmail.com>
2022-01-04 22:58:08 +0530

    created

---
+++ Level_1/Basics/Starters/Even_Number.java
@@ -0,0 +1,16 @@
+import java.util.*
+
+public class Main{
+
+public static void main(String [] args) {
+
+int x;
+
+if ( x% 2 === 0)
+{
+System.out.println(x + " is even");
+}
+else {
+System.out.println(x + " is odd");
+}
+}
