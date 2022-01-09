914729e8a90e03d4fa3e61202eb8344d043d8017
Ayush Aggarwal <Aama098@gmail.com>
2022-01-09 19:08:36 +0530

    update

--- Level_1/Basics/Starters/fibonacci.java
+++ Level_1/Basics/Starters/fibonacci.java
@@ -5,7 +5,7 @@ import java.util.*;
   public static void main(String[] args) {
       Scanner scn = new Scanner(System.in);
       int n = scn.nextInt();
-      // fibonnaci series upto n
+      // loop will run n times
       int a =0;
       int b=1;
       for (int i=1;i<=n;i++){
