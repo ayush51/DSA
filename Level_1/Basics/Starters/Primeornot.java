2c140972a1ddd2cfe776f509f257b60aa5406626
Ayush Aggarwal <Aama098@gmail.com>
2022-01-09 17:32:19 +0530

    code

---
+++ Level_1/Basics/Starters/Primeornot.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main{
+    
+public static void main(String[] args) {
+ 
+Scanner scn = new Scanner(System.in);
+int t = scn.nextInt();
+
+for(int i = 0; i<t ; i++){
+    int n = scn.nextInt();
+
+int count = 0;
+    for(int div = 1; div<=n; div++){
+        if (n % div == 0){
+            count++;
+        }
+        }
+        
+        if ( count == 2){
+            System.out.println("prime")   ; 
+            }
+            else {
+                System.out.println("not prime");
+            }
+            
+        }
+        
+    }
+
+}
+
+// Complexity of above is t*n = 10^4*10^9 = 10^13 --- hence time limit exceeded
+
+import java.util.*;
+
+public class Main{
+    
+public static void main(String[] args) {
+ 
+Scanner scn = new Scanner(System.in);
+int t = scn.nextInt();
+
+for(int i = 0; i<t ; i++){
+    int n = scn.nextInt();
+
+int count = 0;
+    for(int div = 2; div * div <=n; div++){
+        if (n % div == 0){
+            count++;
+        }
+        }
+        
+        if ( count == 0){
+            System.out.println("prime")   ; 
+            }
+            else {
+                System.out.println("not prime");
+            }
+            
+        }
+        
+    }
+
+}
+
+
+
+/// complexity 10^4 * root(10^9) = 10^9 
+
+
+
+ 
