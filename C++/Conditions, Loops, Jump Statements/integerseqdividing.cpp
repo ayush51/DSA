1af6fb9b693da81be8e75415c95bfc1f8a7d2167
Ayush Aggarwal <Aama098@gmail.com>
2022-05-29 23:25:14 +0530

    code
    https://codeforces.com/contest/1102/problem/A

---
+++ C++/Conditions, Loops, Jump Statements/integerseqdividing.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    long long int n;
+    cin >> n;
+    if(n%4==1||n%4==2)
+    {
+        cout << 1<< endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+    return 0;
+}
+
