7503f7dcc4c0f8417f9ecde6d9d56d88deaf1b34
Ayush Aggarwal <Aama098@gmail.com>
2022-06-22 00:29:42 +0530

    create

---
+++ C++/array/removeduplicatesfromsortedarray1&2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums){
+        if (nums.size()==0)
+            return 0;
+        
+        int i = 0;
+        for(int j =1 ; j<nums.size(); j++){
+            if(nums[j] != nums[i]){
+                i++;
+                    nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+};
+
+/////////////////////////
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+        int i = 0;
+        for(auto e:nums){
+            if(i==0|| i==1|| nums[i-2]!=e){
+                nums[i]=e;
+                i++;
+            }
+        }
+        return i;
+    }
+};
