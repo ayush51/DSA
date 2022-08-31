6cdff7c41ac017c668c9a46702609147c0a0e679
Ayush Aggarwal <Aama098@gmail.com>
2022-08-31 00:22:27 -0400

    update

--- C++/array/howmanyevennumberofdisigts.cpp
+++ C++/array/howmanyevennumberofdisigts.cpp
@@ -9,3 +9,13 @@ class Solution:
             if counter%2 == 0:
                 result +=1;
         return result;
+
+
+def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for num in nums:
+            s = str(num)
+            if len(s) % 2 == 0:
+                count += 1
+        return count
+        
