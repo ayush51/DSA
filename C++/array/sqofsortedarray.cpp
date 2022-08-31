86b54017f06ad8e0372ba48333fd4793e1646c92
Ayush Aggarwal <Aama098@gmail.com>
2022-08-31 00:23:14 -0400

    create

---
+++ C++/array/sqofsortedarray.cpp
@@ -0,0 +1,9 @@
+Squares of a Sorted Array
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(0, len(nums)):
+            nums[i] = nums[i] * nums[i];
+        nums.sort();
+        return nums;
+        
