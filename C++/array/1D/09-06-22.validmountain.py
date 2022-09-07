b50c5b81bbf56d07529aef00f0cbb39051a1fb3b
Ayush Aggarwal <Aama098@gmail.com>
2022-09-06 20:46:45 -0400

    create

---
+++ C++/array/1D/09-06-22.validmountain.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        strictlyInc = False
+        strictlyDec = False
+        for i in range (len(arr)-1):
+            if arr[i+1]<arr[i]:
+                if strictlyInc == False:
+                    return False
+                strictlyDec = True
+            elif arr[i+1]>arr[i]:
+                if strictlyDec == True:
+                    return False
+                strictlyInc = True
+            else: 
+                return False
+            
+        if strictlyInc and strictlyDec:
+            return True
+        else:
+            return False
