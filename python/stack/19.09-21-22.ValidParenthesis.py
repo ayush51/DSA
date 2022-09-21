1670c1bb6baaae0e93b68b0697c2fae0aed539a1
Ayush Aggarwal <Aama098@gmail.com>
2022-09-21 01:13:06 -0400

    20validparenthesis

---
+++ python/stack/19.09-21-22.ValidParenthesis.py
@@ -0,0 +1,12 @@
+def isValid(self, s:str)->bool
+    stack = []
+    closetoOpen = {"}":"{", ")": "(", "]":"["}
+        for i in s:
+            if i in closetoOpen:
+                if stack and stack[-1] == closetoOpen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
