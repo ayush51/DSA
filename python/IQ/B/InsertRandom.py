2d9c3fa12ca2169f6fe21a26c1d31e17456e0e04
Ayush Aggarwal <Aama098@gmail.com>
2022-09-23 20:14:19 -0400

    cr 380

---
+++ python/IQ/B/InsertRandom.py
@@ -0,0 +1,22 @@
+def __init__(self):
+    self.numMap = {}
+    self.numListt = []
+def insert(self, val:int)->bool:
+    res = val not in self.numMap:
+        if res:
+            self.numMap[val]=len(self.numList)
+            self.numList.appen[val]
+        return res
+def remove(self, val: int)->bool:
+    res = val in self.numMap
+    if res:
+        idx = self.numMap[val]
+        lastval = self.numList[-1]
+        self.numList[idx] = lastval
+        self.numList.pop()
+        self.numMap[lastval] = idx
+        del self.numMap[val]
+    return res
+ def getrandom(self)->int:
+   return random.choice(self.numList)
+        
