9f85c9604b2ecf7025c44b8271124d1c9d86bc4c
Ayush Aggarwal <Aama098@gmail.com>
2022-09-27 23:52:35 -0400

    cr

--- python/IQ/B/920 Meetingrooms.py
+++ python/IQ/B/920 Meetingrooms.py
@@ -1,1 +1,8 @@
-
+intervals.sort(Key = lambda i: i.start)
+for i in range(intervals[1, len(intervals))
+                         
+  i1 = intervals[i-1]
+                       i2 = intervals[i]
+                         if i1.end < i2.start
+                         return false
+        return true
