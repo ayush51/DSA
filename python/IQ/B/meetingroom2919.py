d9705df3ce327661dbf5b2b38230c784b479275d
Ayush Aggarwal <Aama098@gmail.com>
2022-09-27 23:55:32 -0400

    cr

---
+++ python/IQ/B/meetingroom2919.py
@@ -0,0 +1,18 @@
+start = sorted([i.start for i in intervals])
+end = sorted([e.start for e in intervals])
+
+res, count = 0, 0
+s,e = 0,0
+
+while s<len(intervals):
+  if start[s]<end[e]
+      s+=1
+      count +=1
+      
+  else: 
+    e+=1
+    count -=1
+    
+    res = max(res, count)
+    
+    ret res
