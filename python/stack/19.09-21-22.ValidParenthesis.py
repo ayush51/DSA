16a09ea389fb124c30080a0f7b48f5203d1ba269
Ayush Aggarwal <Aama098@gmail.com>
2022-09-29 04:21:18 -0400

    upd

--- python/stack/19.09-21-22.ValidParenthesis.py
+++ python/stack/19.09-21-22.ValidParenthesis.py
@@ -2,8 +2,8 @@ def isValid(self, s:str)->bool
     stack = []
     closetoOpen = {"}":"{", ")": "(", "]":"["}
         for i in s:
-            if i in closetoOpen:
-                if stack and stack[-1] == closetoOpen[i]:
+            if i in closetoOpen:  #if this char is a closing parenthesis - every key, we wanna make sure our stack is not empty and the value at top of stack is matching opening parenthes
+                if stack and stack[-1] == closetoOpen[i]:  #here closetoOpen refers to the matching opening parenthesis 
                     stack.pop()
                 else:
                     return False
