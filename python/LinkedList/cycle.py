6a0d5dcdc1099bdc2a61a33970427a79093bc019
Ayush Aggarwal <Aama098@gmail.com>
2022-10-07 23:59:31 -0400

    u

--- python/LinkedList/cycle.py
+++ python/LinkedList/cycle.py
@@ -8,7 +8,7 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if head == None or head.next == None:
             return False
-        slow = head
+        slow = headhh
         fast = head
         while fast.next!=None and fast.next.next != None:
             fast = fast.next.next
