e4133c2fe811963b2418f1c9f07359533e9cd460
Ayush Aggarwal <Aama098@gmail.com>
2022-10-12 01:10:35 -0400

    recursive

--- python/array/reverse.py
+++ python/array/reverse.py
@@ -10,3 +10,18 @@ def reverse(arr:int):
 
 
 reverse([1,2,4,5])
+
+---------------------
+
+def reverse(arr:list[int], l, r):
+    if l==r:
+        return
+    arr[l], arr[r] = arr[r], arr[l]
+    
+    reverse(arr, l+1, r-1)
+    print(arr)
+    
+
+reverse([1,2,3,4,5], 0, 4)
+
+
