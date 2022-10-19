dfd454142110a91910492de88d8b25a1216bfecb
Ayush Aggarwal <Aama098@gmail.com>
2022-10-18 22:18:29 -0400

    up

--- python/IQ/B/InsertRandom.py
+++ python/IQ/B/InsertRandom.py
@@ -2,16 +2,16 @@ def __init__(self):
     self.numMap = {}
     self.numListt = []
 def insert(self, val:int)->bool:
-    res = val not in self.numMap:
+    res = val not in self.numMap: #if val not in map
         if res:
             self.numMap[val]=len(self.numList)
             self.numList.appen[val]
         return res
 def remove(self, val: int)->bool:
-    res = val in self.numMap
+    res = val in self.numMap #if val in map
     if res:
         idx = self.numMap[val]
-        lastval = self.numList[-1]
+        lastval = self.numList[-1]   #intro lastval cz popping it in 0(1)
         self.numList[idx] = lastval
         self.numList.pop()
         self.numMap[lastval] = idx
