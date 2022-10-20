0309f4698686381e2849f93bf1befe3286bdf0f0
Ayush Aggarwal <Aama098@gmail.com>
2022-10-19 23:26:04 -0400

    up

--- python/IQ/B/findnships.py
+++ python/IQ/B/findnships.py
@@ -36,5 +36,32 @@ class Solution:
             return topLeftQ + bottomRightQ + topRightQ + bottomLeftQ
         return findShips(topRight, bottomLeft)
             
+        
+        
+        class Solution:
+    def countShips(self, sea: 'Sea', tR: 'Point', bL: 'Point') -> int:
+        def findShips(tR, bL):
+            if tR.x < bL.x or tR.y < bL.y:
+                return 0
+            elif  tR.x == bL.x or tR.y == bL.y:
+                return int(sea.hasShips(tR,bL))
+            
+            if not sea.hasShips(tR, bL):
+                return 0
+            
+            midX = (tR.x + bL.x)//2
+            midY = (tR.y + bL.y)//2
+            mid = Point(midX, midY)
+            
+            topLeftQ = findShips(Point(mid.x, tR.y), Point(bL.x, mid.y+1))
+            topRightQ = findShips(tR, Point(mid.x+1, mid.y+1))
+            bottomLeftQ = findShips(Point(mid.x,mid.y), bL)
+            bottomRightQ = findShips(Point(tR.x,mid.y), Point(mid.x+1, bL.y))
+            
+            return topLeftQ + topRightQ + bottomLeftQ + bottomRightQ
+        return findShips(tR, bL)
+            
+    
+        
     
         
