f970096badc5b55f47a2a903241197f5e221ddbd
Ayush Aggarwal <Aama098@gmail.com>
2022-10-21 16:10:12 -0400

    up

--- python/IQ/B/dailytemp.py
+++ python/IQ/B/dailytemp.py
@@ -9,4 +9,25 @@ class Solution:
                     break
                 
         return res
+    
+    
+    O(n)
+    
+    class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        
+        stack = [] #temp, indexoftemp : calc the no of days for greater temp
+        
+        
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]: #stack non empty  and stack top is LESS than temp
+                stackT, stackI = stack.pop()       #then pop from stack and we calc index - stackI AND APPEND TO RES
+                res[stackI] = (i-stackI)
+            stack.append([t,i])
+        
+        
+                
+        return res
+        
         
